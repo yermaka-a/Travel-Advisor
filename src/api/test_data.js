8a161a203b1712e8ceb46762148091f2e96276d3
YermakA <ermak.080@mail.ru>
2024-07-26 02:35:11 +0800

    feat: delete README.md

---
+++ src/api/test_data.js
@@ -0,0 +1,116 @@
+const testData = [
+  {
+    "xid": "N1436114956",
+    "name": "Троицкий собор",
+    "rate": 3,
+    "osm": "node/1436114956",
+    "wikidata": "Q4411464",
+    "kinds": "religion,other_temples,interesting_places",
+    "point": {
+      "lon": 103.87093353271484,
+      "lat": 52.528018951416016,
+    },
+  },
+  {
+    "xid": "N2126013973",
+    "name": "Самородок детский клуб",
+    "rate": 1,
+    "osm": "node/2126013973",
+    "kinds": "other,unclassified_objects,interesting_places,tourist_object",
+    "point": {
+      "lon": 103.85769653320312,
+      "lat": 52.51123809814453,
+    },
+  },
+  {
+    "xid": "N2126537295",
+    "name": "Ветеранам Великой Отечественной",
+    "rate": 1,
+    "osm": "node/2126537295",
+    "kinds": "historic,burial_places,interesting_places,war_memorials",
+    "point": {
+      "lon": 103.86161041259766,
+      "lat": 52.512168884277344,
+    },
+  },
+  {
+    "xid": "N2126537427",
+    "name": "Спасская часовня",
+    "rate": 1,
+    "osm": "node/2126537427",
+    "kinds": "religion,other_temples,interesting_places",
+    "point": {
+      "lon": 103.86402130126953,
+      "lat": 52.51213836669922,
+    },
+  },
+  {
+    "xid": "N5923510430",
+    "name": 'Цветочная клумба "Кувшин"',
+    "rate": 1,
+    "osm": "node/5923510430",
+    "kinds": "cultural,urban_environment,interesting_places,installation",
+    "point": {
+      "lon": 103.87960052490234,
+      "lat": 52.511436462402344,
+    },
+  },
+  {
+    "xid": "W202704005",
+    "name": "Старое кладбище",
+    "rate": 1,
+    "osm": "way/202704005",
+    "kinds": "cemeteries,historic,burial_places,interesting_places",
+    "point": {
+      "lon": 103.85833740234375,
+      "lat": 52.52016067504883,
+    },
+  },
+  {
+    "xid": "N2126573380",
+    "name": "Перспектива",
+    "rate": 1,
+    "osm": "node/2126573380",
+    "kinds": "other,unclassified_objects,interesting_places,tourist_object",
+    "point": {
+      "lon": 103.860595703125,
+      "lat": 52.51829528808594,
+    },
+  },
+  {
+    "xid": "N6180967506",
+    "name": "В.С.Высоцкому",
+    "rate": 1,
+    "osm": "node/6180967506",
+    "kinds":
+      "historic,monuments_and_memorials,cultural,urban_environment,interesting_places,sculptures,monuments",
+    "point": {
+      "lon": 103.86466979980469,
+      "lat": 52.52524185180664,
+    },
+  },
+  {
+    "xid": "N2032780679",
+    "name": "Церковь Байкальская",
+    "rate": 1,
+    "osm": "node/2032780679",
+    "kinds": "religion,other_temples,interesting_places",
+    "point": {
+      "lon": 103.88622283935547,
+      "lat": 52.51810836791992,
+    },
+  },
+  {
+    "xid": "N3141740760",
+    "name": "святым Петру и Февронии Муромским",
+    "rate": 1,
+    "osm": "node/3141740760",
+    "kinds": "historic,monuments_and_memorials,interesting_places,monuments",
+    "point": {
+      "lon": 103.87046813964844,
+      "lat": 52.52699661254883,
+    },
+  },
+]
+
+export default testData
